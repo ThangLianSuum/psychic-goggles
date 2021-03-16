aca29bf73a517849a28f52f5dbcc38a62f7fdb29
ThangLianSuum <75762682+ThangLianSuum@users.noreply.github.com>
2021-03-16 16:25:37 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,25 @@
 prediction_1 = ""
 prediction_2 = ""
 
-Web.set({
+Webcam.set({
     width:350,
     height:300,
     image_format : 'png',
     png_quality:90
-});
+  });
 
 camera = document.getElementById("camera");
 
-Webcam.attach("#camera");
+Webcam.attach('#camera');  
 
-
-function take_shapshot()
+      
+function take_snapshot()
 {
-     Webcam.snap(function(data_uri) {
-document.getElementById("result").innerHTML = '<img if="captured_image" src="'+data_uri+'"/>';
-     });
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+    });
 }
+
 
 console.log("ml5.version", ml5.version);
 
@@ -35,3 +36,56 @@ function speak(){
     var utterThis = new SpeechSynthesisUtterance(speak_data_1 + speak_data_2);
     synth.speak(utterThis)
 }
+
+function check()
+  {
+    img = document.getElementById('captured_image');
+    classifier.classify(img, gotResult);
+  }
+
+  function gotResult(error, results) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(results);
+      document.getElementById("result_emotion_name").innerHTML = results[0].label;
+      document.getElementById("result_emotion_name2").innerHTML = results[1].label;
+      prediction_1 = results[0].label;
+      prediction_2 = results[1].label;
+      speak();
+      if(results[0].label == "OK")
+      {
+        document.getElementById("update_emoji").innerHTML = "&#128076;";
+      }
+      if(results[0].label == "FIst")
+      {
+        document.getElementById("update_emoji").innerHTML = "&#9994;";
+      }
+      if(results[0].label == "HIVE five")
+      {
+        document.getElementById("update_emoji").innerHTML = "&#9995;";
+      }
+      if(results[0].label == "zztuzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
+      {
+          document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+
+      if(results[1].label == "OK")
+      {
+        document.getElementById("update_emoji").innerHTML = "&#128076;";
+      }
+      if(results[1].label == "FIst")
+      {
+        document.getElementById("update_emoji").innerHTML = "&#9994;";
+      }
+      if(results[1].label == "HIVE five")
+      {
+        document.getElementById("update_emoji").innerHTML = "&#9995;";
+      }
+      if(results[1].label == "zztuzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
+      {
+          document.getElementById("update_emoji").innerHTML = "&#128077;";
+      }
+    }
+  }
+  
